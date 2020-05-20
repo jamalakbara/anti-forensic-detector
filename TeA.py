f133db4961b2902a67f67c1891eaf7f5c572331e
Chairil Akbar <jamalakbaralam@gmail.com>
2020-05-20 23:29:37 +0700

    first commit

---
+++ TeA.py
@@ -0,0 +1,66 @@
+import subprocess
+import os
+import pandas as pd
+import re
+
+try:
+    BASE_DIR = os.getcwd()
+    os.chdir("platform-tools")
+except FileNotFoundError:
+    print("""
+    ===================GAADA FOLDER PLATFORM-TOOLS===================
+   ===================EXTRACT DULU KE DIRECTORY INI===================""")
+else:
+    if not os.path.exists(os.path.join(BASE_DIR, "hasil_backup")):
+        os.mkdir(os.path.join(BASE_DIR, "hasil_backup"))
+
+    if not os.path.exists(os.path.join(BASE_DIR, "list_package")):
+        os.mkdir(os.path.join(BASE_DIR, "list_package"))
+
+    namaBckup = input("Masukin nama file backup : ")
+
+    print("""
+    ===================Tunggu Sebentar Lagi Nge-Backup Nanti Juga Beres===================
+         ===================Oiya Jangan Dicabut Hp-nya Nanti Error===================""")
+    print()
+
+    adbCmd = subprocess.run(f"adb backup -apk -shared -all -f ../hasil_backup/{namaBckup}.ab", shell=True)
+
+    if adbCmd.returncode == 0:
+        if os.path.exists(f"../hasil_backup/{namaBckup}.ab"):
+            print("""
+               ===================Tunggu Sebentar Lagi Buat List Package===================
+            ===================Oiya Jangan Dicabut Lagi Hp-nya Nanti Error===================""")
+            adbCmd = subprocess.run(f"adb shell pm list package > ../list_package/list.txt", shell=True)
+            print()
+            os.chdir("../")
+            if os.path.exists("list_package/list.txt"):
+                listPackage = pd.read_excel("data_set/list package.xlsx")
+
+                packages = pd.read_csv("list_package/list.txt", delimiter=" ", header=None)
+                packages.rename(columns={
+                    0: "Nama Package"
+                }, inplace=True)
+
+                dict_app = {
+                    "Nama App": list(),
+                    "Nama Package": list()
+                }
+                for index in packages.index:
+                    cek = re.search(r"package:(\w+\..+)", packages.loc[index, "Nama Package"])
+                    for i in listPackage.index:
+                        try:
+                            if cek.group(1).strip() == listPackage.loc[i, "package"].strip():
+                                dict_app["Nama App"].append(listPackage.loc[i, "name apk"].strip())
+                                dict_app["Nama Package"].append(listPackage.loc[i, "package"].strip())
+                        except AttributeError:
+                            pass
+                else:
+                    tabel_list_app = pd.DataFrame(dict_app)
+                    print(tabel_list_app)
+            else:
+                print("===================Gagal Bikin List===================")
+
+        else:
+            print("===================Gagal Backup===================")
+
